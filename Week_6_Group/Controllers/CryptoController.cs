0b42144ac3f499942ecd740fabb7764fa4e4011f
Martin-Schendel <13mschen@gmail.com>
2022-02-17 14:36:26 -0600

    working on the CryptoController

---
+++ Week_6_Group/Controllers/CryptoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Week_6_Group.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CryptoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get(string symbol)
+        {
+            return Ok();
+        }
+    }
+
+    public class root
+    {
+        public string timezone { get; set; }
+        public int serverTime { get; set; }
+        //public RateLimits rateLimits { get; set; }
+
+
+    }
+}
