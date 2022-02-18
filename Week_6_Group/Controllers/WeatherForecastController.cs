29ba0299f681e5ec15e213023d851c2805739578
Martin-Schendel <13mschen@gmail.com>
2022-02-18 17:44:07 -0600

    Refactored WeatherForecastController
    I moved the classes around so the higher levels of abstractions come first, so you can read from most to least abstract.

--- Week_6_Group/Controllers/WeatherForecastController.cs
+++ Week_6_Group/Controllers/WeatherForecastController.cs
@@ -26,58 +26,50 @@ namespace Week_6_Group.Controllers
                 return BadRequest(ex.Message);
             }
 
-
-            Root list = JsonConvert.DeserializeObject<Root>(result);
+            Root? list = JsonConvert.DeserializeObject<Root>(result);
             return Ok(list);
         }
-
-        public class Temp
-        {
-            public int c { get; set; }
-            public int f { get; set; }
-        }
-
-        public class Wind
-        {
-            public int km { get; set; }
-            public int mile { get; set; }
-        }
-
-        public class CurrentConditions
-        {
-            public string dayhour { get; set; }
-            public Temp temp { get; set; }
-            public string precip { get; set; }
-            public string humidity { get; set; }
-            public Wind wind { get; set; }
-            public string comment { get; set; }
-        }
-
-        public class MaxTemp
-        {
-            public int c { get; set; }
-            public int f { get; set; }
-        }
-
-        public class MinTemp
-        {
-            public int c { get; set; }
-            public int f { get; set; }
-        }
-
-        public class NextDay
-        {
-            public string day { get; set; }
-            public string comment { get; set; }
-            public MaxTemp max_temp { get; set; }
-            public MinTemp min_temp { get; set; }
-        }
-
         public class Root
         {
-            public string region { get; set; }
-            public CurrentConditions currentConditions { get; set; }
-            public List<NextDay> next_days { get; set; }
+            public string? region { get; set; }
+            public CurrentConditions? currentConditions { get; set; }
+            public List<NextDay>? next_days { get; set; }
+        }
+        public class CurrentConditions
+        {
+            public string? dayhour { get; set; }
+            public Temp? temp { get; set; }
+            public string? precip { get; set; }
+            public string? humidity { get; set; }
+            public Wind? wind { get; set; }
+            public string? comment { get; set; }
+        }
+        public class NextDay
+        {
+            public string? day { get; set; }
+            public string? comment { get; set; }
+            public MaxTemp? max_temp { get; set; }
+            public MinTemp? min_temp { get; set; }
+        }
+        public class Temp
+        {
+            public int? c { get; set; }
+            public in?t f { get; set; }
+        }
+        public class Wind
+        {
+            public int? km { get; set; }
+            public int? mile { get; set; }
+        }
+        public class MaxTemp
+        {
+            public int? c { get; set; }
+            public int? f { get; set; }
+        }
+        public class MinTemp
+        {
+            public int? c { get; set; }
+            public int? f { get; set; }
         }
     }
 }
